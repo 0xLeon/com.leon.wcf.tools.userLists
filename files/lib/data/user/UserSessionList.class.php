396a3e474889311ef9722c6ededff2e849370ba5
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-12-15 17:57:01 +0100

    another missing semicolon

--- files/lib/data/user/UserSessionList.class.php
+++ files/lib/data/user/UserSessionList.class.php
@@ -21,7 +21,7 @@ class UserSessionList extends UserList {
 		$this->sqlSelects .= "		GROUP_CONCAT(DISTINCT languages.languageID ORDER BY languages.languageID ASC SEPARATOR ',') AS languageIDs, ";
 		$this->sqlJoins .= " LEFT JOIN	wcf".WCF_N."_user_to_groups groups ";
 		$this->sqlJoins .= " ON		(groups.userID = user.userID) ";
-		$this->sqlJoins .= " LEFT JOIN	wcf".WCF_N."_user_to_languages languages "
+		$this->sqlJoins .= " LEFT JOIN	wcf".WCF_N."_user_to_languages languages ";
 		$this->sqlJoins .= " ON		(languages.userID = user.userID) ";
 		$this->sqlGroupBy .= "user.userID";
 	}
